15f6158cea687c22bbef5fd0557dff03e2b275f4
Bananymous <oskari.alaranta@bananymous.com>
2026-07-05 00:11:33 +0300

    ports/doom: Update and install to /usr/share/games

--- ports/doom/build.sh
+++ ports/doom/build.sh
@@ -2,7 +2,7 @@
 
 NAME='doom'
 VERSION='git'
-DOWNLOAD_URL="https://github.com/ozkl/doomgeneric.git#5041246e859052e2e258ca6edb4e1e9bbd98fcf5"
+DOWNLOAD_URL="https://github.com/ozkl/doomgeneric.git#dcb7a8dbc7a16ce3dda29382ac9aae9d77d21284"
 DEPENDENCIES=('sdl2-compat' 'SDL2_mixer' 'timidity')
 
 configure() {
@@ -14,13 +14,13 @@ build() {
 		wget https://distro.ibiblio.org/slitaz/sources/packages/d/doom1.wad -O ../doom1.wad || exit 1
 	fi
 
-	CFLAGS='-std=c11' make --directory doomgeneric --file Makefile.sdl CC="$CC" SDL_PATH="$BANAN_SYSROOT/usr/bin/" || exit 1
+	make -C doomgeneric -f Makefile.sdl CC="$CC" SDL_PATH="$BANAN_SYSROOT/usr/bin/" || exit 1
 }
 
 install() {
 	mkdir -p "$DESTDIR/usr/bin"
 	cp -vf doomgeneric/doomgeneric "$DESTDIR/usr/bin/doom"
 
-	mkdir -p "$DESTDIR/home/user"
-	cp -vf ../doom1.wad "$DESTDIR/home/user/"
+	mkdir -p "$DESTDIR/usr/share/games/doom"
+	cp -vf ../doom1.wad "$DESTDIR/usr/share/games/doom/"
 }
