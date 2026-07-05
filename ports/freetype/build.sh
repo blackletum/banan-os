659bc50aecd53892008a157c8a7db3e3eb902837
Bananymous <oskari.alaranta@bananymous.com>
2026-07-05 13:34:27 +0300

    ports: Update and dont build static freetype

--- ports/freetype/build.sh
+++ ports/freetype/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash ../install.sh
 
 NAME='freetype'
-VERSION='2.14.1'
-DOWNLOAD_URL="https://download.savannah.gnu.org/releases/freetype/freetype-$VERSION.tar.xz#32427e8c471ac095853212a37aef816c60b42052d4d9e48230bab3bdf2936ccc"
+VERSION='2.14.3'
+DOWNLOAD_URL="https://download.savannah.gnu.org/releases/freetype/freetype-$VERSION.tar.xz#36bc4f1cc413335368ee656c42afca65c5a3987e8768cc28cf11ba775e785a5f"
 DEPENDENCIES=('zlib' 'libpng')
-CONFIG_SUB=('builds/unix/config.sub')
 CONFIGURE_OPTIONS=(
+	'--disable-static'
 	'lt_cv_deplibs_check_method=pass_all'
 )
