db8544065e39e0ddae15efdd208dc66d89cf076c
Bananymous <oskari.alaranta@bananymous.com>
2026-07-05 13:06:32 +0300

    ports: Don't build static zlib and zstd

--- ports/zlib/build.sh
+++ ports/zlib/build.sh
@@ -5,10 +5,11 @@ VERSION='1.3.2'
 DOWNLOAD_URL="https://www.zlib.net/zlib-$VERSION.tar.gz#bb329a0a2cd0274d05519d61c667c062e06990d72e125ee2dfa8de64f0119d16"
 
 configure() {
-	cmake --fresh -B build -S . -G Ninja \
+	cmake --fresh -S . -B build -G Ninja \
 		--toolchain="$BANAN_TOOLCHAIN_DIR/Toolchain.txt" \
-		-DCMAKE_INSTALL_PREFIX=/usr \
+		-DCMAKE_INSTALL_PREFIX='/usr' \
 		-DCMAKE_BUILD_TYPE=Release \
+		-DZLIB_BUILD_STATIC=OFF \
 		|| exit 1
 }
 
--- ports/zstd/build.sh
+++ ports/zstd/build.sh
@@ -5,9 +5,14 @@ VERSION='1.5.7'
 DOWNLOAD_URL="https://github.com/facebook/zstd/releases/download/v$VERSION/zstd-$VERSION.tar.gz#eb33e51f49a15e023950cd7825ca74a4a2b43db8354825ac24fc1b7ee09e6fa3"
 
 configure() {
-	cmake --fresh -B _build -S build/cmake -G Ninja \
+	cmake --fresh -S build/cmake -B _build -G Ninja \
 		--toolchain="$BANAN_TOOLCHAIN_DIR/Toolchain.txt" \
-		-DCMAKE_INSTALL_PREFIX=/usr \
+		-DCMAKE_INSTALL_PREFIX='/usr' \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DBUILD_SHARED_LIBS=ON \
+		-DZSTD_PROGRAMS_LINK_SHARED=ON \
+		-DZSTD_BUILD_SHARED=ON \
+		-DZSTD_BUILD_STATIC=OFF \
 		 || exit 1
 }
 
